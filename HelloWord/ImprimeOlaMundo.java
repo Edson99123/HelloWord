816033c868b05653552fc229eba36ae4433ef4e2
Edson Sales Sales <edsonsales99@gmail.com>
2021-02-15 16:21:57 -0500

    Primeiro commit no reposotório

---
+++ HelloWord/ImprimeOlaMundo.java
@@ -0,0 +1,12 @@
+/*Aurtor:Edson Sales
+ * Data: 15/02/2021*/
+package HelloWord;
+
+public class ImprimeOlaMundo {
+	public static void main(String[] args) {
+		WelloWord impreiMemensagem = new WelloWord(); // Instancia da classe WelloWord sendo chamada
+		impreiMemensagem.imprimeOla(); //Chamada do metodo para imprimir o metodo
+	
+	}
+
+}
---
+++ HelloWord/WelloWord.java
@@ -0,0 +1,10 @@
+/*Autor: Edson Sales 
+ * Data: 15/02/2021*/
+package HelloWord;
+
+public class WelloWord {
+	public void imprimeOla() {
+		System.out.println("Esse e um olá mundo em java!");
+	}
+
+}
